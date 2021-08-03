75951fd4598fec05f3e8aeda5f304a090bc4bba2
PrincessPride <bukhareal@gmail.com>
2021-08-04 04:52:02 +0600

    "beta column" error

--- about_page.py
+++ about_page.py
@@ -6,10 +6,8 @@ from PIL import Image
 
 def show_devs():
     st.write("# Hello!")
-    col1, col2, col3 = st.beta_columns(3)
-    with col1:
-        st.image("data//s.png", width = 200)  
-        st.write("## I'm **Saif**") 
+    st.image("data//s.png", width = 200)  
+    st.write("## I'm **Saif**")  
     st.header("I'm a fellow student @ **Daffodil International University**")
     st.write("### This is my first Machine Learning Project, **PROJECT : Salary-Prediction**. \
     Salary-Prediction is based on the data collected from various StackOverflow users of the year 2020.")
