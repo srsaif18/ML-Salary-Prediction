ad13c147de7fee3f465be41214388f1b4ad77c46
PrincessPride <bukhareal@gmail.com>
2021-08-04 10:07:53 +0600

    added new certificates

--- about_page.py
+++ about_page.py
@@ -17,3 +17,5 @@ def show_devs():
     st.image("data//Python for beginners - Learn all the basics of python - 2.jpg", width = 800)
     st.image("data//Programming_Hero_Certificate_fundamental.jpg", width = 800)
     st.image("data//cert-17238243-1157.png", width = 800)
+    st.image("data//Python Complete Course For Python Beginners.png", width = 800)
+    st.image("data//Beginner's guide to Competitive programming using python.png", width = 800)
