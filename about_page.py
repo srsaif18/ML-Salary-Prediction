4fc480b827da7125be1d62c55f7df50c85283dfe
PrincessPride <bukhareal@gmail.com>
2021-08-04 11:16:51 +0600

    experiment 2 :)

--- about_page.py
+++ about_page.py
@@ -23,13 +23,7 @@ def show_devs():
 
 
 print("""
-Here's our logo (hover to see the title text):
-
-Inline-style: 
-![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")
-
-Reference-style: 
-![alt text][logo]
-
-[logo]: https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 2"
+Hi
+Hello
+Bye
 """)
