e01ce204d4ce82f10409fde714fee192c7e06f2b
PrincessPride <bukhareal@gmail.com>
2021-08-04 12:48:57 +0600

    experiment 7

--- about_page.py
+++ about_page.py
@@ -12,17 +12,18 @@ def show_devs():
     st.write("### This is my first Machine Learning Project, **[PROJECT : Salary-Prediction](https://github.com/PrincessPride/ML-Salary-Prediction)**. \
     Salary-Prediction is based on the data collected from various StackOverflow users of the year 2020.")
 
+    st.latex("My")
     st.latex("Online")
-    st.latex("Certifications :")
-    st.write("[]()")
+    st.latex("Certifications")
+    st.write("## *[Beginner's guide to Competitive programming using python](https://udemy-certificate.s3.amazonaws.com/image/UC-20d9bb53-682c-4d88-8330-a64b838ab686.jpg)* ##")
     st.image("data//Beginner's guide to Competitive programming using python.jpg", width = 800)
-    st.write("# *[Python Complete Course For Python Beginners](https://udemy-certificate.s3.amazonaws.com/image/UC-664e5e16-a084-420a-a07f-82ed54398168.jpg)* #")
+    st.write("## *[Python Complete Course For Python Beginners](https://udemy-certificate.s3.amazonaws.com/image/UC-664e5e16-a084-420a-a07f-82ed54398168.jpg)* ##")
     st.image("data//Python Complete Course For Python Beginners.jpg", width = 800)
-    
+    st.write("## *[Python for beginners - Learn all the basics of python](https://udemy-certificate.s3.amazonaws.com/image/UC-95476dbd-0f1b-4fdb-a686-13dfbd451b72.jpg)* ##")
     st.image("data//Python for beginners - Learn all the basics of python.jpg", width = 800)
-    
+    st.write("## *[Python for beginners - Learn all the basics of python](https://udemy-certificate.s3.amazonaws.com/image/UC-87988046-8bab-4027-8446-de9750cf5555.jpg)* ##")
     st.image("data//Python for beginners - Learn all the basics of python - 2.jpg", width = 800)
-    st.write("# [Python Course 2021](https://www.programming-hero.com/) #")
+    st.write("## *[Python Course 2021](https://www.programming-hero.com/)* ##")
     st.image("data//Programming_Hero_Certificate_fundamental.jpg", width = 800)
-    st.write("# [Python for Beginners](https://www.sololearn.com/certificates/course/en/17238243/1157/landscape/png) #")
+    st.write("## *[Python for Beginners](https://www.sololearn.com/certificates/course/en/17238243/1157/landscape/png)* ##")
     st.image("data//cert-17238243-1157.png", width = 800)
